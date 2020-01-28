d5fdaf9afd1409777eeeff88138a25ff2fd1b9b8
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-01-28 19:16:30 +0000

    This is where we will do our project. The main file has been added as a solution already, this means we can add more files as we progress through.

---
+++ Main_File/Main_File.cpp
@@ -0,0 +1,10 @@
+//Main_File.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//If you want to run the file just press Ctrl + F5. Debugging can be found in the tabs above.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome!\n";
+}
