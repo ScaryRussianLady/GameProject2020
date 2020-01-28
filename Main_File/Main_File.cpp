e9e897369230cfa7984324af8216bab086669583
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-01-28 19:26:12 +0000

    Just put an example of how the function could work whilst interacting with the user. Also added relevant comments.

--- Main_File/Main_File.cpp
+++ Main_File/Main_File.cpp
@@ -2,9 +2,18 @@
 //If you want to run the file just press Ctrl + F5. Debugging can be found in the tabs above.
 
 #include <iostream>
+#include <conio.h> //Functions for console input and output. Can change stuff like the background colour, text colour, etc.
+//conio documentation: https://www.includehelp.com/c-tutorial/c-language-conio-h-in-c-programming-tutorial.aspx
+#include <stdlib.h> //Used for getting the input from the user and output the result back to the monitor. 
 using namespace std;
 
 int main()
 {
-    cout << "Welcome!\n";
+    //Simply takes in the users name and returns this.
+    char name[25];
+    cout << "What is your name?" << endl;
+    cin.getline(name, 25);
+    cout << "Nice to meet you, " << name << ". Hope to see you around more!" << endl;
+    cout << "\n----------------------Press any key to continue----------------------" << endl;
+    return 0;
 }
