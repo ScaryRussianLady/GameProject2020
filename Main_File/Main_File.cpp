c46cd07fddd87864245cef8df23c543726292dfe
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-02-06 00:11:56 +0000

    Produced a very simple function to demonstrate the structure that will be required for the game.

--- Main_File/Main_File.cpp
+++ Main_File/Main_File.cpp
@@ -5,15 +5,20 @@
 #include <conio.h> //Functions for console input and output. Can change stuff like the background colour, text colour, etc.
 //conio documentation: https://www.includehelp.com/c-tutorial/c-language-conio-h-in-c-programming-tutorial.aspx
 #include <stdlib.h> //Used for getting the input from the user and output the result back to the monitor. 
-using namespace std;
+//Please try to avoid using 'using namespace std;' if possible in all of your files as this is a bad habit. Avoiding this habit will be more beneficial for the future.
 
 int main()
 {
-    //Simply takes in the users name and returns this.
-    char name[25];
-    cout << "What is your name?" << endl;
-    cin.getline(name, 25);
-    cout << "Nice to meet you, " << name << ". Hope to see you around more!" << endl;
-    cout << "\n----------------------Press any key to continue----------------------" << endl;
+    //I produced a very simple function where the player is able to give the name of their clan and which side they want to be on.
+    //This should be used as a template to begin the game and give everyone a sense of how this shoud be structured.
+    char nameOfClan[25];
+    std::cout << "The Gladiator" << std::endl;
+    std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "\nWhat would you like to name your clan, Chief?" << std::endl;
+    std::cin.getline(nameOfClan, 25);
+    std::cout << "\nAs the Chief of " << nameOfClan << " you must decide whether you will be Attack or Defence." << std::endl;
+    char typeOfClan[25];
+    std::cout << "\nWould you like to pick Attack or Defence?" << std::endl;
+    std::cin.getline(typeOfClan, 25);
     return 0;
 }
