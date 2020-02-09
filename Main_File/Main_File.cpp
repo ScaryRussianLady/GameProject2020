981de34deeaa691104cec5acaeaf221f6eb59fbf
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-02-09 23:11:07 +0000

    Began adjusting introduction to print out line by line.

--- Main_File/Main_File.cpp
+++ Main_File/Main_File.cpp
@@ -15,6 +15,7 @@
 //I hate this project already
 //God, same
 
+//[Annija Balode 9102828] and referenced from https://www.dreamincode.net/forums/topic/228382-make-text-to-appear-letter-by-letter-in-console/
 int main()
 {
 
@@ -23,16 +24,28 @@ int main()
 
     system("color 8F");//changes colour of terminal and text. couldn't get conio to work. use "color %" to view all available colours
     char nameOfClan[25];
+    char userName[25];
     std::cout << "The Gladiator" << std::endl;
     std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
-    std::cout << "Sir! Emperor Macrinus is setting up New camps for Gladiators to train in!" << std::endl;
-    std::cout << "It says on this rock here that he has put you in charge of this camp,\nyour first order is to give it a name... so" << std::endl;
+    char str[] = { "Welcome to The Gladiator\nWhat is your name, Chief?\n" };
+    int length = strlen(str);
+    for (int i = 0; i < length; i++)
+    {
+        putchar(str[i]);
+        Sleep(80);
+    }
+    //std::cout << "Welcome to The Gladiator.\nWhat is your name, Chief?" << std::endl;
+    std::cin.getline(userName, 25);
+    system("CLS");
+    std::cout << userName << "! Emperor Macrinus is setting up new camps for Gladiators to train in!" << std::endl;
+    std::cout << "It says on this rock here that he has now put you in charge of this camp,\nyour first order is to give it a name..." << std::endl;
     std::cout << "\nWhat would you like to name your clan, Chief?" << std::endl;
     std::cin.getline(nameOfClan, 25);
     std::cout << "\nAs the Chief of " << nameOfClan << " you must decide whether you will be Attack or Defence." << std::endl;
     char typeOfClan[25];
-    std::cout << "\nWould you like to pick Attack or Defence?" << std::endl;
+    std::cout << "\nWould you like your clan to be Attack or Defence?" << std::endl;
     std::cin.getline(typeOfClan, 25);
+
 
 
     system("CLS");//is used to clear the text on the terminal. conio's clrscn() did not work
