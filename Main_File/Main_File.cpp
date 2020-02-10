92bfcedcee26b4d19bba022663615ce87ca4c8a6
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-02-10 00:00:19 +0000

    Outputs the introduction letter by letter.

--- Main_File/Main_File.cpp
+++ Main_File/Main_File.cpp
@@ -11,9 +11,21 @@
 //Please try to avoid using 'using namespace std;' if possible in all of your files as this is a bad habit. Avoiding this habit will be more beneficial for the future.
 //#include <sqlite3.h>
 //#include <stdio.h>
-//Slight error with importing SQLite but will fix this later.
-//I hate this project already
-//God, same
+#include <thread>
+
+void singularWordOutput(const std::string& text)
+{
+    // loop through each character in the text
+    for (std::size_t i = 0; i < text.size(); ++i)
+    {
+        // output one character
+        // flush to make sure the output is not delayed
+        std::cout << text[i] << std::flush;
+
+        // sleep 60 milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(80));
+    }
+}
 
 //[Annija Balode 9102828] and referenced from https://www.dreamincode.net/forums/topic/228382-make-text-to-appear-letter-by-letter-in-console/
 int main()
@@ -27,23 +39,24 @@ int main()
     char userName[25];
     std::cout << "The Gladiator" << std::endl;
     std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
-    char str[] = { "Welcome to The Gladiator\nWhat is your name, Chief?\n" };
-    int length = strlen(str);
-    for (int i = 0; i < length; i++)
-    {
-        putchar(str[i]);
-        Sleep(80);
-    }
+    singularWordOutput("Welcome to The Gladiator.\nWhat is your name, Chief?\n");
+    //char str[] = { "Welcome to The Gladiator\nWhat is your name, Chief?\n" };
+    //int length = strlen(str);
+    //for (int i = 0; i < length; i++)
+    //{
+    //    putchar(str[i]);
+      //  Sleep(70);
+    //}
+
     //std::cout << "Welcome to The Gladiator.\nWhat is your name, Chief?" << std::endl;
     std::cin.getline(userName, 25);
-    system("CLS");
-    std::cout << userName << "! Emperor Macrinus is setting up new camps for Gladiators to train in!" << std::endl;
-    std::cout << "It says on this rock here that he has now put you in charge of this camp,\nyour first order is to give it a name..." << std::endl;
-    std::cout << "\nWhat would you like to name your clan, Chief?" << std::endl;
+    singularWordOutput(std::string(userName) + "! Emperor Macrinus is setting up new camps for Gladiators to train in!\n");
+    singularWordOutput("It says on this rock here that he has now put you in charge of this camp,\nyour first order is to give it a name...\n");
+    singularWordOutput("\nWhat would you like to name your clan, Chief " + std::string(userName) + "?\n");
     std::cin.getline(nameOfClan, 25);
-    std::cout << "\nAs the Chief of " << nameOfClan << " you must decide whether you will be Attack or Defence." << std::endl;
+    singularWordOutput("\nAs the official Chief of " + std::string(nameOfClan) + " you must decide whether you will be Attack or Defence.\n");
     char typeOfClan[25];
-    std::cout << "\nWould you like your clan to be Attack or Defence?" << std::endl;
+    singularWordOutput("\nWould you like to be Attack or Defence?");
     std::cin.getline(typeOfClan, 25);
 
 
