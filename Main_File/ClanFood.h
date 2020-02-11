7af83257976454bf43bee9d14e84cd9aee0b44b4
RishikeshS13 <56678998+RishikeshS13@users.noreply.github.com>
2020-02-11 13:27:40 +0000

    Attempting to have the Food and Hydrations levels

---
+++ Main_File/ClanFood.h
@@ -0,0 +1,21 @@
+void ClanLvls(){
+int W = 100, F = 100 
+    while (F >= 0) {
+		if (c == "levels") {
+			system("CLS");//clear screen
+			cout << "Your Water level is " << W << endl;
+			cout << "Your Hunger level is " << F << endl;
+			std::cout << "Available options: " << std::endl;
+			std::cout << "\n1. Eat Food";
+			std::cout << "\n2. Drink" << std::endl;
+			std::cout << "\nUser choice: ";
+			std::string choice;
+			std::cin >> choice;
+			std::cout << "*action depending on user choice*";
+		}
+			
+		if (c == "levels" && MC == 1) {
+			cout << "\n";
+			cout << "Your Water level is " << W << endl;
+			cout << "Your Hunger level is " << F << endl;
+		}
