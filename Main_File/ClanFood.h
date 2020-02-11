3cb9dd86e20d6cd9072f1090740e5a1203a345e9
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-02-11 17:29:48 +0000

    Idk what change this is.

--- Main_File/ClanFood.h
+++ Main_File/ClanFood.h
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include <stdlib.h>
 void ClanLvls(){
-	//Annija fixed some of the code for this header file.
+	//Annija fixed some of Rishikesh's errors.
 	int W = 100, F = 100;
     while (F >= 0) {
 		if (c == "levels") {
@@ -10,16 +10,15 @@ void ClanLvls(){
 			std::cout << "Your Water level is " << W << std::endl;
 			std::cout << "Your Hunger level is " << F << std::endl;
 			std::cout << "Available options: " << std::endl;
-			std::cout << "\n1. Eat Food" << std::endl;
+			std::cout << "\n1. Eat Food";
 			std::cout << "\n2. Drink" << std::endl;
-			std::cout << "\nUser choice: " << std::endl;
+			std::cout << "\nUser choice: ";
 			std::string choice;
 			std::cin >> choice;
 			std::cout << "*action depending on user choice*" << std::endl;
 		}
 			
-		//if (c == "levels" && MC == 1) {
-			//cout << "\n";
-			//cout << "Your Water level is " << W << endl;
-			//cout << "Your Hunger level is " << F << endl;
-		
+		if (c == "levels" && MC == 1) {
+			std::cout << "\nYour Water level is " << W << std::endl;
+			std::cout << "Your Hunger level is " << F << std::endl;
+		}
