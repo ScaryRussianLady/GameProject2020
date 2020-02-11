645fb032d4b9ddd1d62fa543a2a82a13d6aa82f3
RishikeshS13 <56678998+RishikeshS13@users.noreply.github.com>
2020-02-11 17:53:22 +0000

    emptypush

--- Main_File/ClanFood.h
+++ Main_File/ClanFood.h
@@ -6,7 +6,7 @@ void ClanFood(){
 	int F = 100;
     while (F >= 0) {
 		if (F > 100) {
-			system("CLS");//clear screen
+			system("CLS");// clear screen
 			//std::cout << "Your Water level is " << W << std::endl;
 			std::cout << "Your Hunger level is " << F << std::endl;
 			//std::cout << "Available options: " << std::endl;
