a5a13708868fd872d971a5b2c0576efa64df0649
Christian <I-Dont-Dev@users.noreply.github.com>
2020-02-17 22:29:58 +0000

    Start the fight calculation function

---
+++ Main_File/fightCalc.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+// Required headerfiles to calculate the fighters
+#include "Gladiator.h"
+
+bool fightCalculation(gladiator Fighter) {
+	std::cout << Fighter.hp << std::endl;
+}
