1de0587186ddded60e70f158bfef4a0db488e2ee
Callum <Callum.jones5@btinternet.com>
2020-02-18 23:14:03 +0000

    newer method of training gladiators

--- Main_File/GladiatorTraining New.h
+++ Main_File/GladiatorTraining New.h
@@ -3,25 +3,14 @@
 #include <GameClasses.h>
 
 // [Callum Jones 9406128] Function to train the basic stats of a gladiator
-int trainGladiator() {
+int trainGladiator(Gladiator& gladiator) {
 	bool trained = false;
-	int gladChoice = 0;
 	int statChoice = 0;
-	// need a list of players current gladiators from the player class for now will call Player.gladiators
-	std::cout << "Please Choose a the number of the gladiator to train from these options";
-	for (int i = 0; i < Player.numOfGladiators; i++) {
-		std::cout << i << ":" << Player.gladiators[i].nickname << std::endl;
-	}
-	std::cin >> gladChoice;
-	while (gladChoice < 1 & gladChoice > Player.numOfGladiators) {
-		std::cout << "Please enter a valid choice" << std::endl;
-		std::cin >> gladChoice;
-	}
 
 	std::cout << "These are your chosen gladiators current stats";
-	std::cout << Player.gladiators[gladChoice - 1].strength;
-	std::cout << Player.gladiators[gladChoice - 1].agility;
-	std::cout << Player.gladiators[gladChoice - 1].dexterity;
+	std::cout << gladiator.strength;
+	std::cout << gladiator.agility;
+	std::cout << gladiator.dexterity;
 
 	while (statChoice < 1 & statChoice > 3) {
 		std::cout << "Please choose a stat to train from 1:Strength 2:agility 3:Dexterity" << std::endl;
@@ -30,22 +19,22 @@ int trainGladiator() {
 	std::cout << "Ok your chosen stat will be trained";
 
 	if (statChoice == 1) {
-		Player.gladiators[gladChoice].increaseStrength(2);
-		std::cout << "This gladiators strength stat is now" << Player.gladiators[gladChoice].strength;
+		gladiator.increaseStrength(2);
+		std::cout << "This gladiators strength stat is now" << gladiator.strength;
 		trained = true;
 	}
 	else if (statChoice == 2) {
-		Player.gladiators[gladChoice].increaseAgility + 0.05;
-		std::cout << "This gladiators agility stat is now" << Player.gladiators[gladChoice].agility;
+		gladiator.increaseAgility + 0.05;
+		std::cout << "This gladiators agility stat is now" << gladiator.agility;
 		trained = true;
 	}
 	else if (statChoice == 3) {
-		Player.gladiators[gladChoice].increaseDexterity + 0.05;
-		std::cout << "This gladiators dexterity stat is now" << Player.gladiators[gladChoice].dexterity;
+		gladiator.increaseDexterity + 0.05;
+		std::cout << "This gladiators dexterity stat is now" << gladiator.dexterity;
 		trained = true;
 	}
 	if (trained == true) {
-		Player.gladiators[gladChoice].adjustHunger(-10);
-		Player.gladiators[gladChoice].adjustThirst(-15);
+		gladiator.adjustHunger(-10);
+		gladiator.adjustThirst(-15);
 	}
 	return gla;
