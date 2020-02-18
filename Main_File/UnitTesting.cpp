c2bd5ba4e7f1afffc7aec4f11a59f23ca1100eb9
RishikeshS13 <56678998+RishikeshS13@users.noreply.github.com>
2020-02-18 13:09:22 +0000

    Attempting different test options to fit the test table i have made

--- Main_File/UnitTesting.cpp
+++ Main_File/UnitTesting.cpp
@@ -2,3 +2,33 @@
 #include "fightCalc.h"
 #include "timeTillFight.h"
 #include "GameClasses.h	"
+#include"Gladitor.h"
+#include"GladiatorTraining.h"
+#include"goldManagement.h"
+#include"goldManagement.h"
+#include"healthManagement.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramwork;
+
+namespace Mysolutions#
+
+    TEST_METHOD(methodName)
+{
+    // test method body
+}
+
+TEST_METHOD(methodName)
+{
+    // test method body
+}
+
+TEST_CLASS_INITIALIZE(methodName)
+{
+    // test class initialization  code
+}
+
+
+BEGIN_TEST_CLASS_ATTRIBUTE(testClassName)
+TEST_CLASS_ATTRIBUTE(attributeName, attributeValue)
+...
+END_TEST_CLASS_ATTRIBUTE()
