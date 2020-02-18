52b6e104ef9a53434ae3755813349112cd159fa3
Christian <I-Dont-Dev@users.noreply.github.com>
2020-02-18 04:13:56 +0000

    Made a gladitor class with stats based off of Jamie's

--- Main_File/GameClasses.h
+++ Main_File/GameClasses.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <stdlib.h> 
 
 // [START OF CODE BY: CHRISTIAN ]
 
@@ -66,3 +67,72 @@ public:
 
 // [END OF CODE BY: CHRISTIAN ]
 
+// [START OF CODE BY: CHRISTIAN ]
+
+class Gladiator {
+private:
+
+	std::string firstname;
+	std::string surname;
+#
+	unsigned char hp;
+	unsigned char hunger;
+	unsigned char thirst;
+
+	void setUp() {
+		hp = 100;
+		hunger = 100;
+		thirst = 100;
+
+		// Going to set up a random name generator
+		std::string firstnameList[28] = { "Augustus", "Cassius", "Cicero", "Domitius", "Felix", "Hadrianus", "Marcellus", "Hortensius",
+		"Julius", "Marianus", "Maximus", "Octavius", "Titus", "Valentinus", "Vitus", "Valerius", "Albus", "Antonius",
+		"Caesar", "Blasius", "Brutus", "Crispus", "Decimus", "Festus", "Germanus", "Marcus", "Maximus", "Septimius" };
+
+		std::string surnameList[24] = { "Gnaeus", "Flavius", "Pustula", "Lucius", "Calidius", "Maius", "Primus", "Statilius",
+			"Comitianus", "Manius", "Memmius", "Ferentinus", "Gallio", "Aquillius", "Gaurus", "Sextus", "Calidius", "Pilus",
+			"Arruns", "Pomponius", "Aponius", "Appius", "Sornatius", "Narcissus" };
+
+		firstname = firstnameList[rand() % 27 + 0];
+		surname = surnameList[rand() % 24 + 0];
+
+	}
+
+public:
+
+	std::string nickname;
+
+	// Gladiator's stats that'll be used during battles
+	unsigned int strength;
+	unsigned int defence;
+	unsigned int agility;
+	unsigned int dexterity;
+
+	Gladiator() {
+
+		strength = rand() % 10 + 5;
+		defence = rand() % 10 + 5;
+		agility = rand() % 10 + 5;
+		dexterity = rand() % 10 + 5;
+
+		setUp();
+	}
+
+	void changeNickname(std::string newName) {
+		nickname = newName;
+	}
+
+	std::string getName(std::string nameType) {
+		if (nameType == "firstname") {
+			return firstname;
+		}
+		else if (nameType == "surname") {
+			return surname;
+		}
+		else if (nameType == "full") {
+			return firstname + " " + surname;
+		}
+	}
+};
+
+// [END OF CODE BY: CHRISTIAN ]
