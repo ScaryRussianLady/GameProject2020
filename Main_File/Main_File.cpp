e5814df9f7bef3d5c68d6d6b17b49bc8276f0841
Annija Balode <35531855+ScaryRussianLady@users.noreply.github.com>
2020-02-25 22:11:48 +0000

    No major changes. Just tidying up.

--- Main_File/Main_File.cpp
+++ Main_File/Main_File.cpp
@@ -3,36 +3,13 @@
 
 //IMPORTANT NOTICE: This is a link to how to install vcpkg, if you are unable to follow it, drop me a message (Annija) https://www.youtube.com/watch?v=wRnjahwxZ8A.
 
-//#include "timeTillFight.h"
 #include <windows.h>
 #include <iostream>
 //conio documentation: https://www.includehelp.com/c-tutorial/c-language-conio-h-in-c-programming-tutorial.aspx
-#include <stdlib.h> //Used for getting the input from the user and output the result back to the monitor. 
 //Please try to avoid using 'using namespace std;' if possible in all of your files as this is a bad habit. Avoiding this habit will be more beneficial for the future.
-#include <sqlite3.h>
-#include <stdio.h>
-#include <thread>
-#include <conio.h>
-#include <locale>
-#include <string>
-#include <cstring>
-#include <cctype>
 #include "GameClasses.h"
-#include <sstream>
-#include <fstream>
-#include <limits>
 #include "MainScreen.h"
 #include "Introduction.h"
-
-
-void centerstring(char* s)
-{
-    int l = strlen(s);
-    int pos = (int)((80 - l) / 2);
-    for (int i = 0; i < pos; i++)
-        std::cout << " " << std::endl;
-    std::cout << s << std::endl;
-}
 
 //Beginning of code by [Annija Balode 9102828].
 int main()
@@ -43,4 +20,5 @@ int main()
 
     return (0);
 }
+
 
