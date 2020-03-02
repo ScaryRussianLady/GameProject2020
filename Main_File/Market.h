ee8ad0bce1e568f125e9b205b363e666397a9f16
Callum <Callum.jones5@btinternet.com>
2020-03-02 13:12:18 +0000

    created a function for visitng market and added basics of buying food and water

---
+++ Main_File/Market.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <Windows.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sstream>
+#include <conio.h>
+#include <string.h>
+#include "timeTillFight.h"
+#include "GameClasses.h"
+
+// shields, weapons, food, water, medical, supplies
+
+
+int Market(Player& player) {
+
+	int playeritemchoice = 0;
+	int itemAmmount = 0;
+	int cost;
+	std::cout << "Welcome to The Market";
+	while (playeritemchoice < 1 or playeritemchoice > 4) {
+		std::cout << "What can we help you with today 1:Food 2:Water 3:Weapons 4:Medical Supplies";
+		std::cin >> playeritemchoice;
+	}
+	if (playeritemchoice == 1) {
+		std::cout << "So your after some food, one stack of 5 food will cost you 2 gold, want to continue";
+		while (itemAmmount > 0 && player.amountFood + (itemAmmount * 5) > 100) {
+			std::cout << "How many stacks of food would you like";
+			std::cin >> itemAmmount;
+		}
+		cost = itemAmmount * 2;
+		player.amountGold = player.amountGold - cost;
+		player.amountFood = player.amountFood + (itemAmmount * 5);
+	}
+	if (playeritemchoice == 2) {
+		std::cout << "So your after some food, one barrel of 10 water will cost you 2 gold, want to continue";
+		while (itemAmmount > 0 && player.amountFood + (itemAmmount * 10) > 100) {
+			std::cout << "How many stacks of food would you like";
+			std::cin >> itemAmmount;
+		}
+		cost = itemAmmount * 2;
+		player.amountGold = player.amountGold - cost;
+		player.amountWater = player.amountWater + (itemAmmount * 10);
+
+	}
+}
+
+
+	
