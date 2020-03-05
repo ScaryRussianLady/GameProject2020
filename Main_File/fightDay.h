8abdd8dd6d65462246bbef5eb2012f9b059eef0c
Christian <I-Dont-Dev@users.noreply.github.com>
2020-03-05 20:48:38 +0000

    Working on actually loading the gladiator

--- Main_File/fightDay.h
+++ Main_File/fightDay.h
@@ -41,6 +41,8 @@ void fightDay(int userid) {
 
 	std::string yesNo;
 
+	gladiatorData gladData;
+
 	std::cout << std::setfill('+') << std::setw(60) << "FIGHT DAY";
 	std::cout << std::setfill('+') << std::setw(59) << "+" << std::endl;
 
@@ -54,12 +56,14 @@ void fightDay(int userid) {
 		// Verification goes here
 		std::cout << "You have selected:\n" << std::endl;
 		showGladiators(userid, std::stoi(gladiatorChoice));
-		std::cout << "Is this the gladiator you wanted to select? YES[1] NO[0]\n" << std::endl;
+		std::cout << "Is this the gladiator you wanted to select?\nYES[1] NO[0]\n" << std::endl;
 		std::cin >> yesNo;
 
 		system("CLS");
 
 		if (std::stoi(yesNo) == 1) {
+			gladData = importGladiatorData(userid, std::stoi(gladiatorChoice));
+			std::cout << "This the dude" << gladData.plrId << std::endl;
 			break;
 		}
 	}
--- Main_File/gladiatorListing.h
+++ Main_File/gladiatorListing.h
@@ -45,6 +45,109 @@ int printGladiators(void* NotUsed, int argc, char** argv, char** azColName) {
     return 0;
 }
 
+struct gladiatorData {
+    int gladId;
+    int plrId;
+
+    std::string fn;
+    std::string ls;
+    std::string nn;
+
+    int hp;
+    int hng;
+    int thr;
+
+    int stg;
+    int def;
+    int agi;
+    int dex;
+};
+
+int gladiatorCounter2 = 0;
+int selNum2;
+gladiatorData gladStruct;
+
+int getGladData(void* NotUsed, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++) {
+
+        if (i % 9 != 0) {
+            continue;
+        }
+
+        gladiatorCounter2++;
+
+        if (selNum2 != 0) {
+            if (gladiatorCounter2 != selNum2) {
+                continue;
+            }
+        }
+
+        //gladiatorData gladStruct;
+
+        gladStruct.gladId = std::stoi(argv[i]);
+
+        gladStruct.plrId = std::stoi(argv[i + 1]);
+
+
+        gladStruct.fn = std::string(argv[i + 2]);
+
+        gladStruct.ls = std::string(argv[i + 3]);
+        gladStruct.nn = std::string(argv[i + 4]);
+
+        gladStruct.hp = std::stoi(argv[i + 5]);
+        gladStruct.hng = std::stoi(argv[i + 6]);
+        gladStruct.thr = std::stoi(argv[i + 7]);
+
+        gladStruct.stg = std::stoi(argv[i + 8]);
+        gladStruct.def = std::stoi(argv[i + 9]);
+        gladStruct.agi = std::stoi(argv[i + 10]);
+        gladStruct.dex = std::stoi(argv[i + 11]);
+
+        //std::cout << "I ahte this" << typeid(gladStruct.gladId).name() << std::endl;
+
+        break;
+
+        std::cout << gladStruct.fn << std::endl;
+    }
+
+    // Return successful
+    return 0;
+}
+
+
+gladiatorData importGladiatorData(int playerID, int selectedNum) {
+   
+    selNum2 = selectedNum;
+ 
+    gladiatorCounter2 = 0;
+
+    std::string Data;
+
+    sqlite3* db;
+    char* zErrMsg = 0;
+    int rc;
+    std::string sql;
+    rc = sqlite3_open("GladiatorDatabase.db", &db);
+
+    if (rc)
+    {
+        std::cout << "Database error: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        //return;
+    }
+
+    sql = "SELECT gladiatorID, playerID, nickname, firstName, surname, hp, hunger, thirst, strength, defence, agility, dexterity FROM PlayerGladiators WHERE playerID =" + std::to_string(playerID) + ";";
+
+    rc = sqlite3_exec(db, sql.c_str(), getGladData, 0, &zErrMsg);
+
+
+    sqlite3_close(db);
+
+    //std::cout << "fuck off " << gladStruct.gladId << std::endl;
+
+    return gladStruct;
+}
+
 void showGladiators(int playerID, int selectedNum) {
 
     
